ac2699d47f6dde8d9d1e0d59a789d1e809ea4f15
est_calexx <10johannesmunoz@gmail.com>
2026-03-17 15:14:04 +0800

    updated: UI design of the Menu screen

--- frontend/lib/screens/menu_screen.dart
+++ frontend/lib/screens/menu_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frontend/components/menu_screen/count_button.dart';
+import 'package:frontend/main.dart';
 import 'package:frontend/models/menu_model.dart';
 
 import '../constants.dart';
@@ -18,7 +20,6 @@ class _MenuScreenState extends ConsumerState<MenuScreen> {
   Widget build(BuildContext context) {
     final menuItems = ref.read(menuProvider);
     // final testProducts = ref.read(productProvider);
-
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 110,
@@ -59,25 +60,62 @@ class _MenuScreenState extends ConsumerState<MenuScreen> {
                       crossAxisCount: 2,
                       mainAxisSpacing: 20,
                       crossAxisSpacing: 20,
-                      childAspectRatio: 1,
+                      childAspectRatio: 0.78,
                     ),
                     itemBuilder: (BuildContext context, int index) {
                       return Container(
                         padding: EdgeInsets.symmetric(vertical: 10),
                         decoration: BoxDecoration(
-                          color: Color(0xFFFF9644).withValues(alpha: 0.50),
+                          color: Color(0xFFEFE2D3),
                           borderRadius: BorderRadius.circular(10),
                         ),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.end,
                           children: [
-                            Text(
-                              menuItems[index].label!,
-                              style: TextStyle(
-                                color: kPrimaryColor,
-                                fontFamily: "flame",
-                                fontSize: 14,
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Color(
+                                    0xFFFF9644,
+                                  ).withValues(alpha: 0.50),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                margin: EdgeInsets.symmetric(horizontal: 10),
+                                width: double.infinity,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      menuItems[index].label!,
+                                      style: TextStyle(
+                                        color: kPrimaryColor,
+                                        fontFamily: "flame",
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      "1.5PHP",
+                                      style: TextStyle(
+                                        color: kPrimaryColor,
+                                        fontFamily: "flame",
+                                        fontSize: 13,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                  ],
+                                ),
                               ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Expanded(child: SizedBox()),
+                                CountButton(symbol: "-"),
+                                Text("0"),
+                                CountButton(symbol: "+"),
+                                Expanded(child: SizedBox()),
+                              ],
                             ),
                           ],
                         ),
@@ -88,11 +126,7 @@ class _MenuScreenState extends ConsumerState<MenuScreen> {
               ),
             ],
           ),
-          Positioned(
-            bottom: 0,
-            child: CartContainer(
-            ),
-          ),
+          Positioned(bottom: 0, child: CartContainer()),
         ],
       ),
     );
